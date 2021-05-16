c6e50396db0d6a0ee17c00f854f40e6c1e2def33
Ayushak18 <aayushkamboj7@gmail.com>
2021-05-16 16:29:40 +0530

    CRUD

---
+++ block-BNaacy/routes/users.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../models/user');
+
+router.get('/new', (req, res) => {
+  res.render('newUser');
+});
+
+router.post('/', (req, res) => {
+  User.create(req.body, (error, data) => {
+    if (error) {
+      console.log(error);
+      res.redirect('/users/new');
+    } else {
+      res.send('User added');
+    }
+  });
+});
+
+module.exports = router;
